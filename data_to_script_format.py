7da69b0e0e609619954bd01383707ff7ecf3d747
racinmat <racinmat@fel.cvut.cz>
2017-11-22 14:56:28 +0100

    Fixed skipping gif and other fully images sharing.

--- data_to_script_format.py
+++ data_to_script_format.py
@@ -16,20 +16,24 @@ def transform_to_script(group_name, data, file_name):
             message = 'added photos'
         elif 'story' in post and post['type'] in ('photo', 'status', 'video'):
             message = post['story']
-        elif 'attachments' in post and len(post['attachments']['data']) > 0 and post['attachments']['data'][0]['type'] == 'unavailable':
+        elif 'attachments' in post and len(post['attachments']['data']) > 0 and \
+                        post['attachments']['data'][0]['type'] == 'unavailable':
             continue
+        elif 'attachments' in post and len(post['attachments']['data']) > 0 and \
+                        post['attachments']['data'][0]['type'] == 'animated_image_share':
+            message = 'some gif is shared'
         else:
             print(post)
             # raise Exception('Weird post')
 
         created_time = fb_to_datetime(post['created_time']).strftime('%d.%m.%Y %H:%M:%S')
-        lines.append('{}: {}: {}'.format(post['from']['name'], created_time, message))
-        for comment in post['comments']:
+        lines.append('{}: {}: {}\n'.format(post['from']['name'], created_time, message))
+        for comment in sorted(post['comments'], key=lambda p: fb_to_datetime(p['created_time']).timestamp()):
             created_time = fb_to_datetime(comment['created_time']).strftime('%d.%m.%Y %H:%M:%S')
-            lines.append('{}: {}: {}'.format(comment['from']['name'], created_time, comment['message']))
-        lines.append('-' * 15)
+            lines.append('{}: {}: {}\n'.format(comment['from']['name'], created_time, comment['message']))
+        lines.append(('-' * 15) + '\n')
     with open(os.path.join(get_binary_dir(group_name), file_name), 'w+', encoding='utf-8') as file:
-        file.writelines(lines=lines)
+        file.writelines(lines)
 
 
 def main():
@@ -46,4 +50,4 @@ if __name__ == '__main__':
 
     main()
 
-    print("Everything denormalized")
+    print("Data transformed to script format")
