f311c1ce941b8ffc75bc05f7fff480290ec86a79
Matěj Račinský <racinsky.matej@seznam.cz>
2019-05-06 22:22:39 +0200

    implemented comments to pandas

--- data_to_csv.py
+++ data_to_csv.py
@@ -6,7 +6,7 @@ import os.path as osp
 import pandas as pd
 
 
-def json_posts_to_pandas(group_name, json_file):
+def json_posts_to_pandas(json_file):
     with open(json_file) as fp:
         data = json.load(fp)
 
@@ -22,9 +22,9 @@ def json_posts_to_pandas(group_name, json_file):
             # should be only 1 attachment
             attachment = post['attachments']['data'][0] if 'attachments' in post else None
             flat_post = {
+                'id': post['id'],
                 'from_id': post['from']['id'],
                 'from_name': post['from']['name'],
-                'id': post['id'],
                 'object_id': post.get('object_id', None),
                 'status_type': post.get('status_type', None),  # getter with defaulting if key is not present
                 'type': post['type'],
@@ -47,22 +47,81 @@ def json_posts_to_pandas(group_name, json_file):
     # df.to_csv(json_file + '.csv')
 
 
+def json_comments_to_pandas(json_file):
+    with open(json_file) as fp:
+        data = json.load(fp)
+
+    flat_comments = []
+
+    for comment in data:
+        # some data preliminary cleaning
+        # I don't care about commerce products
+        try:
+            # should be only 1 attachment
+            flat_comment = {
+                'id': comment['id'],
+                'message': comment['message'],
+                'from_id': comment['from']['id'],
+                'from_name': comment['from']['name'],
+                'created_time': comment['created_time'],
+                'parent_id': comment['parent']['id'] if 'parent' in comment else None,
+                'object_id': comment['object']['id'],
+            }
+            flat_comments.append(flat_comment)
+        except KeyError as e:
+            print('hi')
+
+    df = pd.DataFrame(flat_comments)
+    # df.to_csv(json_file + '.csv')
+
+
+def json_reactions_to_pandas(json_file):
+    with open(json_file) as fp:
+        data = json.load(fp)
+
+    flat_reactions = []
+
+    fields = set()
+    for reaction in data:
+        fields = fields.union(reaction.keys())
+        # some data preliminary cleaning
+        # I don't care about commerce products
+        try:
+            # should be only 1 attachment
+            flat_reaction = {
+                'id': reaction['id'],
+                'message': reaction['message'],
+                'from_id': reaction['from']['id'],
+                'from_name': reaction['from']['name'],
+                'created_time': reaction['created_time'],
+                'parent_id': reaction['parent']['id'] if 'parent' in reaction else None,
+                'object_id': reaction['object']['id'],
+            }
+            flat_reactions.append(flat_reaction)
+        except KeyError as e:
+            print('hi')
+
+    df = pd.DataFrame(flat_reactions)
+    print('hi')
+    # df.to_csv(json_file + '.csv')
+
+
 def data_to_pandas(group_name):
     directory = utils.get_dir(utils.texts_root, group_name, utils.Type.POST)
     existing_months = sorted(set([utils.Month.from_str(s.replace('.json', '')) for s in os.listdir(directory)]))
     for month in existing_months:
-        json_posts_to_pandas(group_name, osp.join(directory, f'{month}.json'))
+        json_posts_to_pandas(osp.join(directory, f'{month}.json'))
 
-    # todo: implement the rest
     directory = utils.get_dir(utils.texts_root, group_name, utils.Type.COMMENT)
     existing_months = sorted(set([utils.Month.from_str(s.replace('.json', '')) for s in os.listdir(directory)]))
     for month in existing_months:
-        json_posts_to_pandas(group_name, osp.join(directory, f'{month}.json'))
+        json_comments_to_pandas(osp.join(directory, f'{month}.json'))
 
+    # todo: implement the rest
     directory = utils.get_dir(utils.texts_root, group_name, utils.Type.REACTION)
     existing_months = sorted(set([utils.Month.from_str(s.replace('.json', '')) for s in os.listdir(directory)]))
     for month in existing_months:
-        json_posts_to_pandas(group_name, osp.join(directory, f'{month}.json'))
+        json_reactions_to_pandas(osp.join(directory, f'{month}.json'))
 
 
 def main():
@@ -71,11 +130,11 @@ def main():
 
 
 if __name__ == '__main__':
-    # utils.texts_root = 'texts_test'
-    utils.texts_root = 'texts'
+    utils.texts_root = 'texts_test'
+    # utils.texts_root = 'texts'
     groups = {
-        # 'scitani_ceskych_a_slovenskych_otaku': '135384786514720',
-        'scitani_ceskych_a_slovenskych_otaku_old': '135384786514720',
+        'scitani_ceskych_a_slovenskych_otaku': '135384786514720',
+        # 'scitani_ceskych_a_slovenskych_otaku_old': '135384786514720',
     }
 
     main()
